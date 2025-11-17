0fa22b0713c7698577dea5ed8b6d6e8cc154215a
wennhmann <info@wennhmann.de>
2025-11-17 17:11:07 +0100

    thread

--- .idea/arearu/src/main.rs
+++ .idea/arearu/src/main.rs
@@ -2,13 +2,13 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(||{
-        for i in 1..10{
-            println!("Hello, from thread{}", i);
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-    for i in 1..30{
+   thread::spawn(|| {
+    for i in 1..20{
+        println!("Hello from spawned thread! {}", i);
+        thread::sleep(Duration::from_secs(1));  
+    }
+   })
+    for i in 1..=30{
         println!("Hello from main thread! {}", i);
         thread::sleep(Duration::from_secs(1));
     }
