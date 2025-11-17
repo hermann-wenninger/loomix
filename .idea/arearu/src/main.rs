d5b1f1c5490f9d8fca90534237a996cdb7f46e44
wennhmann <info@wennhmann.de>
2025-11-17 17:41:13 +0100

    a

--- .idea/arearu/src/main.rs
+++ .idea/arearu/src/main.rs
@@ -1,15 +1,31 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::{Arc, Mutex};
 
 fn main() {
-   thread::spawn(|| {
+    let x = 50;
+ let h1 =  thread::spawn(|| {
     for i in 1..20{
         println!("Hello from spawned thread! {}", i);
-        thread::sleep(Duration::from_secs(1));  
+        thread::sleep(Duration::from_secs(1)); 
+
     }
-   })
+   });
     for i in 1..=30{
         println!("Hello from main thread! {}", i);
         thread::sleep(Duration::from_secs(1));
     }
+  let mut k = Arc::new(Mutex::new(0));
+  let h2 =  thread::spawn(move||{
+        for i in 1..=x{
+            let mut k_guard = k.lock().unwrap();
+            *k_guard += i;
+            println!("Hello from moved spawned thread! {}", i);
+            thread::sleep(Duration::from_secs(1));
+        }
+
+    });
+    h1.join().unwrap();
+    h2.join().unwrap();
+    println!("Final value of k: {}", *k.lock().unwrap());
 }
