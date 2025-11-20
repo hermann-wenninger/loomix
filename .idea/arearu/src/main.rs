51cfc9ca22316b8b90060afc832b402b641d91a9
wennhmann <info@wennhmann.de>
2025-11-20 15:26:07 +0100

    are

--- .idea/arearu/src/main.rs
+++ .idea/arearu/src/main.rs
@@ -1,15 +1,9 @@
-fn main() {
- let mut a: [i8; 5] = [5, 4, 3, 2, 1];
-a[2] = 0;
-println!("a: {a:?}");
-fn get_index() -> usize {
-4
-}
-let index = get_index();
-println!("Index: {index}");
-println!("Element at index {}: {}", index, a[index]);
-let xyz:(i8,i8 bool,i32,String) = (1,2,true,45,"hello".to_string());
-println!("Tuple: {:?}", xyz);
-let (x,y,z,b,s) = xyz;
-println!("Destructured: x={}, y={}, z={}, b={}, s={}", x,y,z,b,s);
+use std::fs;
+use std::path::Path;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+ let file_path = Path::new("data/data.txt");
+ let content = fs::read_to_string(file_path)?;
+ println!("File content: {}", content);
+ Ok(())
 }
